eaf5133b0a51902a4e396144a168d06c0a86bb93
Nikhil <nikhillondhe36@gmail.com>
2019-08-03 02:41:30 +0530

    Create yetAnotherPartitionProblem_M.cpp

---
+++ BASIC PROGRAMMING/Basics of Operators/yetAnotherPartitionProblem_M.cpp
@@ -0,0 +1,19 @@
+//@author Nikhil Londhe
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int MX = 1e5;
+
+int arr[MX+1];
+
+int main(){
+  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+  int t; cin>>t; while(t--){
+    int xr=0,mx,n; cin>>n;
+    for(int i=0;i<n;++i){ cin>>arr[i]; xr^=arr[i]; }
+    mx=xr;
+    for(int i=0,x=0;i<n;++i) x^=arr[i], mx=max(mx,x&(x^xr));
+    cout<<mx<<'\n';
+  }
+  return 0;
+}
