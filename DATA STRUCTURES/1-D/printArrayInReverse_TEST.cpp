22ddd091a3bd8c5d4564eb985293b1a9f672e5c3
Nikhil Londhe <nikhillondhe36@gmail.com>
2019-09-10 22:56:54 +0530

    Update 1-D

---
+++ DATA STRUCTURES/1-D/printArrayInReverse_TEST.cpp
@@ -0,0 +1,10 @@
+//@author Nikhil Londhe
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+  int n; cin>>n; int arr[n]; while(n--) cin>>arr[n];
+  for(auto a:arr) cout<<a<<'\n';
+  return 0;
+}
